de3f6a5b2bb4c8e1591f85063f221452454f9bb3
Rodrigo Martinez <58400562+Roderick1325@users.noreply.github.com>
2021-05-18 20:06:52 -0500

    Revert "Trabajo final"
    This reverts commit c49daa9f1c936aecced30dcb14e92fb0f01112c8.

--- App.py
+++ App.py
@@ -1,75 +1,51 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_mysqldb import MySQL
 
-# initializations
 app = Flask(__name__)
 
-# Mysql Connection
+#MySQL Connection
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'rodrigo'
 app.config['MYSQL_PASSWORD'] = 'Roderick12'
 app.config['MYSQL_DB'] = 'FlaskDb'
 mysql = MySQL(app)
 
-# settings
-app.secret_key = "mysecretkey"
+#Setings
+app.secret_key = 'mysecretkey'
 
-# routes
 @app.route('/')
 def Index():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM contacts')
     data = cur.fetchall()
-    cur.close()
     return render_template('index.html', contacts = data)
 
-@app.route('/add_contact', methods=['POST'])
+@app.route('/add_contact', methods =['POST'])
 def add_contact():
     if request.method == 'POST':
         fullname = request.form['fullname']
         phone = request.form['phone']
         email = request.form['email']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO contacts (fullname, phone, email) VALUES (%s,%s,%s)", (fullname, phone, email))
+        cur.execute('INSERT INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)', (fullname, phone, email))
         mysql.connection.commit()
         flash('Contact Added successfully')
         return redirect(url_for('Index'))
 
-@app.route('/edit/<id>', methods = ['POST', 'GET'])
+@app.route('/edit/<id>')
 def get_contact(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM contacts WHERE id = %s', (id))
     data = cur.fetchall()
-    cur.close()
-    print(data[0])
-    return render_template('edit-contact.html', contact = data[0])
+    return render_template('edit-contact', contact = data[0])
 
-@app.route('/update/<id>', methods=['POST'])
-def update_contact(id):
-    if request.method == 'POST':
-        fullname = request.form['fullname']
-        phone = request.form['phone']
-        email = request.form['email']
-        cur = mysql.connection.cursor()
-        cur.execute("""
-            UPDATE contacts
-            SET fullname = %s,
-                email = %s,
-                phone = %s
-            WHERE id = %s
-        """, (fullname, email, phone, id))
-        flash('Contact Updated Successfully')
-        mysql.connection.commit()
-        return redirect(url_for('Index'))
-
-@app.route('/delete/<string:id>', methods = ['POST','GET'])
+@app.route('/delete/<string:id>')
 def delete_contact(id):
     cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM contacts WHERE id = {0}'.format(id))
+    cur.execute('DELETE FROM contacts WHERE id = {}'.format(id))
     mysql.connection.commit()
     flash('Contact Removed Successfully')
     return redirect(url_for('Index'))
 
-# starting the app
-if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
--- README.md
+++
@@ -1,9 +0,0 @@
-# Screenshot
-![](docs/screenshot.png)
-
-# install dependencies
-- pip install flask
-- pip install flask-mysqldb
-
-# issues
-- sudo apt-get install libmysqlclient-dev
